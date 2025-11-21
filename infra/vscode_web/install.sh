1b48f3c3ca91975f429209f09da379f0071f9019
Harsh-Microsoft <v-hbangera@microsoft.com>
2025-11-21 12:07:52 +0530

    Replace azd init with git clone for installation

--- infra/vscode_web/install.sh
+++ infra/vscode_web/install.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt --user -q
 
-azd init -t microsoft/Conversation-Knowledge-Mining-Solution-Accelerator
+git clone https://github.com/Harsh-Microsoft/Conversation-Knowledge-Mining-Solution-Accelerator.git
